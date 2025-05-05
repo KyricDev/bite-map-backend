a01a1b2b9a1040a797687129b0bb62f2174f2800
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-05-05 13:00:12 +0800

    added express

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        'message': 'success'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('listening')
+})
